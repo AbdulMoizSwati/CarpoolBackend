e8f4a7e15213ab3e045a8da48a77e6f99b7fddba
AbdulMoizSwati <abdulmoizkhan362@gmail.com>
2025-11-10 12:34:51 +0500

    added a process set

--- connection/connection.js
+++ connection/connection.js
@@ -1,9 +1,10 @@
 require('dotenv').config();
 const mongoose = require("mongoose");
 
+const url = process.env.MONGODBCONNECT;
 
-function Connect(){
-mongoose.connect("mongodb://localhost:27017/Nodecarpoll").then(()=>{
+function Connect(url){
+mongoose.connect(process.env.MONGO).then(()=>{
     console.log("Mongo Db is Connected Sucessfully");
 }).catch((err)=>{
     console.log(`error of connection databse ${err}`)
