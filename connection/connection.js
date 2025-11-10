0686377a8a1ebc0f9951f425dd75bca99a4a08e7
AbdulMoizSwati <abdulmoizkhan362@gmail.com>
2025-11-10 12:37:12 +0500

    added a process se

--- connection/connection.js
+++ connection/connection.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const url = process.env.MONGODBCONNECT;
 
 function Connect(url){
-mongoose.connect(process.env.MONGO).then(()=>{
+mongoose.connect(url).then(()=>{
     console.log("Mongo Db is Connected Sucessfully");
 }).catch((err)=>{
     console.log(`error of connection databse ${err}`)
